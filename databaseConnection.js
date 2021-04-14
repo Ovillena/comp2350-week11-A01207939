1c0450a370f8a7b1bc435b63faec190a47962313
Octavio Villena <octaviovillena@hotmail.com>
2021-04-13 22:03:26 -0700

    mod for mongo use

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,31 +1,18 @@
-const mysql = require('mysql2');
-
+const MongoClient = require("mongodb").MongoClient;
 const is_heroku = process.env.IS_HEROKU || false;
+const herokuURI =
+  "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.n1i6g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
-
-const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "lab_example",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
-
+const localURI =
+  "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;";
 if (is_heroku) {
-	var database = mysql.createPool(dbConfigHeroku);
+  let database = new MongoClient(herokuURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} else {
+  let database = new MongoClient(localURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
-else {
-	var database = mysql.createPool(dbConfigLocal);
-}
-
-module.exports = database;
-		
